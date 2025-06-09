5b2f6a1797aaee417d2e0bb3c0bd4d6e815a7b41
Er3nX0 <d88aniel@outlook.com>
2025-06-09 22:52:14 +0200

    Dokończenie

--- app/src/main/java/com/example/wizytauweterynarza/MainActivity.java
+++ app/src/main/java/com/example/wizytauweterynarza/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.wizytauweterynarza;
 
+import static android.view.View.VISIBLE;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +24,10 @@ public class MainActivity extends AppCompatActivity {
     TextView wiekTextView;
     SeekBar wiekSeekBar;
     Spinner gatunekSpinner;
+    Button zatwierdzButton;
+    TextView wypelnioneDaneTextView;
+    EditText imieINazwiskoEditText;
+    EditText celWizytyEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +38,15 @@ public class MainActivity extends AppCompatActivity {
         wiekTextView = findViewById(R.id.wiekTextView);
         wiekSeekBar = findViewById(R.id.wiekSeekBar);
         gatunekSpinner = findViewById(R.id.gatunekSpinner);
+        zatwierdzButton = findViewById(R.id.zatwierdzButton);
+        wypelnioneDaneTextView = findViewById(R.id.wypelnioneDaneTextView);
+        imieINazwiskoEditText = findViewById(R.id.imieINazwiskoEditText);
+        celWizytyEditText = findViewById(R.id.celWizytyEditText);
+        TimePicker godzinaWizytyTimePicker = findViewById(R.id.godzinaWizytyTimePicker);
+        godzinaWizytyTimePicker.setIs24HourView(true);
+        godzinaWizytyTimePicker.setHour(16);
+        godzinaWizytyTimePicker.setMinute(0);
+
 
 
         wiekSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
@@ -59,7 +77,16 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 String selectedUnit = parent.getItemAtPosition(position).toString();
-                wiekTextView.setText("" + selectedUnit);
+                wiekTextView.setText("0");
+                wiekSeekBar.setProgress(0);
+                if(position == 0){
+                    wiekSeekBar.setMax(18); //dla psa
+                } else if (position == 1) {
+                    wiekSeekBar.setMax(20); //dla kota
+                } else if (position == 2){
+                    wiekSeekBar.setMax(9);  //dla swinki morskiej
+                }
+
             }
 
             @Override
@@ -67,5 +94,17 @@ public class MainActivity extends AppCompatActivity {
                 wiekTextView.setText("-");
             }
         });
+
+        zatwierdzButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                wypelnioneDaneTextView.setVisibility(VISIBLE);
+                wypelnioneDaneTextView.setText(
+                        imieINazwiskoEditText.getText() + "," + gatunekSpinner.getSelectedItem().toString() +
+                                "," + wiekSeekBar.getProgress() + "," + celWizytyEditText.getText() + "," +
+                                godzinaWizytyTimePicker.getHour() + ":" + godzinaWizytyTimePicker.getMinute());
+            }
+        });
+
     }
 }
